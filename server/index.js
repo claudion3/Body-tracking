45197a0ccc6247f9bb755767cea20f5bc4129440
claudion3 <claudion3@gmail.com>
2021-09-21 20:52:33 +0200

    fix error

--- server/index.js
+++ server/index.js
@@ -13,10 +13,12 @@ const PORT = process.env.API_PORT || 5000;
 app.use(bodyParser.json());
 app.use('/bodyTruck', BodyTruckRoutes);
 app.use(cors());
+
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
+
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 	});
 }
 

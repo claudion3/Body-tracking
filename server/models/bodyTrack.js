8ee05881abc6b020c3273db4d3925aac1d6c4ec4
claudion3 <claudion3@gmail.com>
2022-12-20 18:36:48 +0100

    strictQuery added

--- server/models/bodyTrack.js
+++ server/models/bodyTrack.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-const BodyTrackSchema = new mongoose.Schema({
-	date: { type: Date, required: true },
-	weight: { type: Number, required: true },
-	hipWidth: { type: Number, required: true },
-});
+
+const BodyTrackSchema = new mongoose.Schema(
+	{
+		date: { type: Date, required: true },
+		weight: { type: Number, required: true },
+		hipWidth: { type: Number, required: true },
+	},
+	{
+		strict: true,
+		strictQuery: false, // Turn off strict mode for query filters
+	},
+);
 const BodyTrack = mongoose.model('bodyTrack', BodyTrackSchema);
 
 module.exports = BodyTrack;

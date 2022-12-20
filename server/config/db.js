b0b8591c1de36dc331ac84a749f7f809f8ae7d74
claudion3 <claudion3@gmail.com>
2022-12-20 21:06:42 +0100

    set url db string

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect((process.env.MONGO_URI).toString(), {
+		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
